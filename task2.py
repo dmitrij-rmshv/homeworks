0d30f1131660dc8f31842647e5085dfd89038c9e
Dmitrij Romashev <romashev.dmitrij@gmail.com>
2020-06-02 22:16:15 +0300

    OOP. Advanced level

--- task2.py
+++ task2.py
@@ -1,22 +1,44 @@
-# 2. Реализовать класс Road (дорога), в котором определить атрибуты: length (длина), width (ширина).
-# Значения данных атрибутов должны передаваться при создании экземпляра класса. Атрибуты сделать защищенными.
-# Определить метод расчета массы асфальта, необходимого для покрытия всего дорожного полотна.
-# Использовать формулу: длина * ширина * масса асфальта для покрытия одного кв метра дороги асфальтом,
-# толщиной в 1 см * число см толщины полотна. Проверить работу метода.
-# Например: 20м * 5000м * 25кг * 5см = 12500 т
+# 2. Реализовать проект расчета суммарного расхода ткани на производство одежды. Основная сущность (класс) этого
+# проекта — одежда, которая может иметь определенное название. К типам одежды в этом проекте относятся пальто и костюм.
+# У этих типов одежды существуют параметры: размер (для пальто) и рост (для костюма). Это могут быть обычные
+# числа: V и H, соответственно.
+# Для определения расхода ткани по каждому типу одежды использовать формулы: для пальто (V/6.5 + 0.5),
+# для костюма (2 * H + 0.3). Проверить работу этих методов на реальных данных.
+# Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания: реализовать
+# абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property.
 
 
-class Road:
-
-    def __init__(self, length, width):
-        self._length = length
-        self._width = width
-
-    def massa(self, consumption=25, thickness=5):
-        self.consumption = consumption
-        self.thickness = thickness
-        return self._width * self._length * self.consumption * self.thickness / 1000
+from abc import ABC, abstractmethod
 
 
-a = Road(5000, 20)
-print(a.massa())
+class Clothes(ABC):
+    @abstractmethod
+    def fabrics_used(self):
+        pass
+
+
+class Coat(Clothes):
+
+    def __init__(self, size):
+        self.v = size
+
+    @property
+    def fabrics_used(self):
+        return self.v / 6.5 + 0.5
+
+
+class Suit(Clothes):
+
+    def __init__(self, size):
+        self.h = size
+
+    @property
+    def fabrics_used(self):
+        return 2 * self.h + 0.3
+
+
+my_coat = Coat(56)
+my_suit = Suit(189)
+
+print(f'The coat requires {my_coat.fabrics_used:.2f} m of fabric')
+print(f'The suit requires {my_suit.fabrics_used:.2f} m of fabric')
